9e355efda0e14649fa7ed603687bada871d26421
Guilherme Assunção <67649872+guilhermeassuncao@users.noreply.github.com>
2021-04-29 18:00:09 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,11 @@ export default class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" /> 
                     <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
+                    
+                     <meta property="og:type" content="website"/>
+                     <meta property="og:title" content="Podcastr | Podcastr de tecnologia!"/>
+                     <meta property="og:description" content="Podcastr para aproximar você ainda mais dos melhores podcasts de tecnologia"/>
+                     <meta property="og:url" content="https://podcastr-guilhermeassuncao.vercel.app/"/>
                 </Head>
                 <body>
                     <Main />
